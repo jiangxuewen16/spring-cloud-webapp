480b6cb0c5cb8c27ec36ddafeb601f3321a68c88
蒋雪文 <445251692@qq.com>
2020-06-08 17:12:26 +0800

    修改服务名称

--- console/src/main/java/com/huiquyun/webapp/console/controller/HomeController.java
+++ console/src/main/java/com/huiquyun/webapp/console/controller/HomeController.java
@@ -1,10 +1,8 @@
 package com.huiquyun.webapp.console.controller;
 
+import com.huiquyun.integral.model.po.User;
 import com.huiquyun.webapp.console.service.RemoteIntegralService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/home")
@@ -19,5 +17,10 @@ public class HomeController {
     @GetMapping("/user/score/{userId}")
     public Integer userScore(@PathVariable Integer userId) {
         return this.remoteIntegralService.getUserScore(userId);
+    }
+
+    @PostMapping("/add/user")
+    public User addUser(@RequestBody User user) {
+        return this.remoteIntegralService.update(user);
     }
 }
